2a74f7f7b2a6a50f7a466108f1a7b41fc2d86c3d
user <kozlova.valeriya.v@gmail.com>
2019-11-08 08:57:44 +0100

    ver.2

--- script.js
+++ script.js
@@ -1,58 +1,67 @@
-let arr = ["BANC", "BUREAU", "CABINET", "CARREAU", "CHAISE", "CLASSE", "CLEF", "COIN", "COULOIR", "DOSSIER", "EAU", "ECOLE", "ENTRER", "ESCALIER", "ETAGERE", "EXTERIEUR", "FENETRE", "INTERIEUR", "LAVABO", "LIT", "MARCHE", "MATELAS", "MATERNELLE", "MEUBLE", "MOUSSE", "MUR", "PELUCHE", "PLACARD", "PLAFOND", "PORTE", "POUBELLE", "RADIATEUR", "RAMPE", "RIDEAU", "ROBINET", "SALLE", "SALON", "SERRURE", "SERVIETTE", "SIEGE", "SIESTE", "SILENCE", "SOL", "SOMMEIL", "SONNETTE", "SORTIE", "TABLE", "TABLEAU", "TABOURET", "TAPIS", "TIROIR", "TOILETTE", "VITRE"];
 
-function play() {
-index = Math.floor(Math.random()*arr.length);
-let string = arr[index];
-let mot = string.split("",);
-let motDiv = [] ;
-let td;
-let count = 0;
-let ind;
-let y;
+    let arr = ["BANC", "BUREAU", "CABINET", "CARREAU", "CHAISE", "CLASSE", "CLEF", "COIN", "COULOIR", "DOSSIER", "EAU", "ECOLE", "ENTRER", "ESCALIER", "ETAGERE", "EXTERIEUR", "FENETRE", "INTERIEUR", "LAVABO", "LIT", "MARCHE", "MATELAS", "MATERNELLE", "MEUBLE", "MOUSSE", "MUR", "PELUCHE", "PLACARD", "PLAFOND", "PORTE", "POUBELLE", "RADIATEUR", "RAMPE", "RIDEAU", "ROBINET", "SALLE", "SALON", "SERRURE", "SERVIETTE", "SIEGE", "SIESTE", "SILENCE", "SOL", "SOMMEIL", "SONNETTE", "SORTIE", "TABLE", "TABLEAU", "TABOURET", "TAPIS", "TIROIR", "TOILETTE", "VITRE"];
 
-document.getElementById("count").innerHTML = " " + count;
-
-for(i = 0; i < mot.length; i ++){
-    td =  document.createElement("td");
-    td.setAttribute("id", i);
-    document.getElementById("row").appendChild(td);
-};
+    let index, string, mot;
+    let motDiv = [];
+    let count = 0;
     const buttons = document.querySelectorAll("button");
 
-    [...buttons].map(button =>{ button.addEventListener("click", e =>{
-        e.preventDefault();
-        const letter = e.target.innerHTML;
-        if (mot.includes(letter)){
-            ind = mot.indexOf(letter);
-            let indices =  [];
-            while(ind != -1) {
-                indices.push(ind);
-                ind = mot.indexOf(letter, ind +1);
+    function play() {
+        document.getElementById("row").innerHTML = "";
+        mot = '';
+        index = Math.floor(Math.random()*arr.length);
+        string = arr[index];
+        mot = string.split("",);
+        monDiv = [];
+        count = 0;
+        let ind;
+        let y;
+        console.log(string);
+
+        document.getElementById("count").innerHTML = " " + count;
+
+        for(i = 0; i < mot.length; i++){
+            let td =  document.createElement("td");
+            td.setAttribute("id", i);
+            document.getElementById("row").appendChild(td);
+        };
+
+        [...buttons].map(button => {
+            button.addEventListener("click", e =>{
+                e.preventDefault();
+                e.target.setAttribute('disabled', 'disabled');
+                const letter = e.target.innerHTML;
+                if (mot.includes(letter)){
+                    ind = mot.indexOf(letter);
+                    for (let i = 0; i < mot.length; i++) {
+                        if ( mot[i] == letter ) {
+                            document.getElementById(i).innerHTML = letter;
+                            motDiv[i] = letter;
+                        }
+                    }
+                    setTimeout(control, 100);
+                } else {
+                    count++;
+                    document.getElementById("count").innerHTML = " " + count;
+            
+                }
+                console.log(motDiv);
+            });
+        });
+        function control(){
+        let word1 = JSON.stringify(mot);
+        let word2 = JSON.stringify(motDiv);
+            if (word1 == word2) {
+                window.alert("Congratulations!!!");
+                if (window.confirm("Would you like to play again?")) {
+                    [...buttons].map(button => {
+                        button.removeAttribute("disabled");
+                    });
+                   setTimeout(play, 100);
+                }
             }
-            for (i = 0; i < indices.length; i++){
-                y = indices[i];
-                motDiv[y] = letter;
-                document.getElementById(y).innerHTML = letter;
-            }
-        control();
-        } else {
-            count++;
-            document.getElementById("count").innerHTML = " " + count;
-    
-        }
-        console.log(motDiv);
-    });
-    });
-    function control(){
-       let word1 = JSON.stringify(mot);
-       let word2 = JSON.stringify(motDiv);
-        if (word1 == word2) {
-            window.alert("Congratulations!!!");
-            if (window.confirm("Would you like to play again?")) {
-                motDiv = [];
-                document.getElementById("row").innerHTML = "";
-                play();
-            }
-        }
+        };
     };
- }; play();
+
+    play();
+});
